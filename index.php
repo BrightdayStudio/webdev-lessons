507c96bd1d7ab6c26d61862c02473f8424988248
Leanne Phillips <brightdaystudio@gmail.com>
2019-08-16 16:45:01 +0100

    setup

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/normalise.css">
+    <link rel="stylesheet" type="text/css" media="screen" href="css/base.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="js/basic.js"></script>
+</head>
+
+<body>
+
+    <main>
+
+    </main>
+
+
+
+
+
+
+</body>
+
+</html>
